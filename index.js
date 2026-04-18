fdd815d147089b1802c2e57c13d5e3a27cb913b7
¿zey <adiciurila1999@gmail.com>
2026-04-18 18:50:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const client = new Client({
         GatewayIntentBits.GuildMembers,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent,
-        GatewayIntentBits.Moderation
+        GatewayIntentBits.GuildModeration
     ]
 });
 
